94f83368d4f6f59bef626431a7bea933bb6258cc
Sam <samaracodes@gmail.com>
2023-08-14 21:36:10 -0400

    deleted unnecessary component file

---
+++ src/components/cars/CarsList.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+
+const CarsList = () => {
+    return (
+        <div>
+            <p>List of cars inventory:</p>
+        </div>
+    )
+}
+
+export default CarsList
