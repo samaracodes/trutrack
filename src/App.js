d8c2e70ed777d532e2c9d9a2fdb0fee99fb57f9e
Sam <samaracodes@gmail.com>
2023-08-14 21:33:50 -0400

    fetched data from api, set state, pass props// fixed dynamic routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import './App.css';
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
-import RepairOrders from './components/repairorders/RepairOrders';
-import RepairOrderId from './components/repairorders/RepairOrderId';
 import Cars from './components/cars/Cars'
 import CarId from './components/cars/CarId'
+import RepairOrders from './components/repairorders/RepairOrders';
+import RepairOrderId from './components/repairorders/RepairOrderId';
 
 
 
 function App() {
+  const [cars, setCars] = useState([])
+  const [repairOrders, setRepairOrders] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/car-inventory")
+    .then((response) => response.json())
+    .then((cars) => setCars(cars))
+  }, [])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/car-inventory")
+    .then((response) => response.json())
+    .then((rO) => setRepairOrders(rO))
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
 
-        <Route path='/cars' element={<Cars />} >
-          <Route path=':carId' element={<CarId />} />
-        </Route>
+        <Route path="/cars" element={<Cars carInventory={cars}/>} />
+        <Route path="/cars/:id" element={<CarId />} />
 
-        <Route path='/repair-orders' element={<RepairOrders />} >
-          <Route path=':repairOrderId' element={<RepairOrderId />} />
-        </Route>
+        <Route path="/repair-orders" element={<RepairOrders repairOrders={repairOrders}/>} />
+        <Route path="/repair-orders/:id" element={<RepairOrderId />} />
+        
       </Routes>
     </div>
   );
