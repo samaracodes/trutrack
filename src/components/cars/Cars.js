31f23f399890ef88598d938cfe3d5af02337a74e
Sam <samaracodes@gmail.com>
2023-08-14 21:37:50 -0400

    accepting props and displaying populated data

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Cars = () => {
+const Cars = ({ carInventory }) => {
+
     return (
         <div>
-            <h1>All Cars</h1>
+            <h1>Car Inventory</h1>
+            
+            <div class="card">
+                {/* <ol class="list-group"> */}
+                    {carInventory.map((car) => 
+                    <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill">
+                            <div class="d-flex w-100 justify-content-between">
+                                
+
+                                <img class="row g-0" src="./f80m3.webp" alt="..." height="100" />
+                                
+                            </div>
+                        
+                    </li>
+                    <li class="list-group-item flex-fill">
+
+                            <p class="mb-1">{car.customerName}</p>
+                    </li>
+                    <li class="list-group-item flex-fill">
+                            <p class="mb-1">{car.year} {car.make} {car.model}</p>
+                    </li>
+                    <li class="list-group-item flex-fill">
+                            <small class="text-body-secondary">VIN: {car.vin}</small>
+                        {/* </Link> */}
+                        </li>
+                        </ul>
+                    )}
+                {/* </ol> */}
+            </div>
         </div>
     )
 }
--- src/components/repairorders/RepairOrders.js
+++ src/components/repairorders/RepairOrders.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const RepairOrders = () => {
+const RepairOrders = ({ repairOrders }) => {
+    
     return (
         <div>
             <h1>Repair Orders</h1>
+            
+            <div class="card">
+                <ul class="list-group">
+                    {repairOrders.map((repairOrder) => 
+                        <Link to={`/repair-orders/${repairOrder.id}`} class="list-group-item list-group-item-action">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">RO#{repairOrder.roNumber}</h5>
+                                <small class="text-body-secondary">Status: OPEN</small>
+                            </div>
+
+                            <img src="https://mysterio.yahoo.com/mysterio/api/B859A8A68C69CD56542617906C43B0B8126C44DBE6B723294FE8126CC983DC08/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/CAC50BMC111A021001.jpg" alt="..." height="100" />
+
+                            <p class="mb-1">{repairOrder.customerName}</p>
+                            <p class="mb-1">{repairOrder.year} {repairOrder.make} {repairOrder.model}</p>
+                            <small class="text-body-secondary">VIN: {repairOrder.vin}</small>
+                        </Link>
+                    )}
+                </ul>
+            </div>
         </div>
     )
 }
