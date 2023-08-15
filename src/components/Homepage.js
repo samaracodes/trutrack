ec7fa7bb8300f835f4bec816441f77920754896e
Sam <samaracodes@gmail.com>
2023-08-14 21:34:58 -0400

    added carousel to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,15 @@ import React from "react";
 const Homepage = () => {
     return (
         <div>
-            <h1>Homepage</h1>
+            <div id="carouselExample" class="carousel slide ">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./bmwf1-pit-crew.jpg" class="d-block w-100"  alt="homepage image of f1 pit crew"/>
+                    </div>
+                </div>
+                
+        
+            </div>
         </div>
     )
 }
