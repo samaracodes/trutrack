477f6942bad130abb5957408022c40450094c05a
Sam <samaracodes@gmail.com>
2023-08-14 21:38:32 -0400

    added navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,30 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <nav class="navbar navbar-expand-sm bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">TruTrack</a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/cars">Car Inventory</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/repair-orders">Repair Orders</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
