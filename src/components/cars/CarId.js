c44856fff153e74bec220f65b2da87422b8242de
Sam <samaracodes@gmail.com>
2023-08-14 21:39:08 -0400

    console log slug params

--- src/components/cars/CarId.js
+++ src/components/cars/CarId.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const CarId = () => {
+    const { slug } = useParams()
+    console.log(slug)
     return (
         <div>
             <h1>Car ID</h1>
