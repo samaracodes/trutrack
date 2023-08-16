c5daf389dcb6b84a8215725a606fc47c80eb2f54
Sam <samaracodes@gmail.com>
2023-08-16 17:50:16 -0400

    setup sidebar for dashboard layout

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -1,39 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Sidebar from "../Sidebar";
 
 const Cars = ({ carInventory }) => {
 
     return (
-        <div>
-            <h1>Car Inventory</h1>
-            
-            <div class="card">
-                {/* <ol class="list-group"> */}
-                    {carInventory.map((car) => 
-                    <ul class="list-group list-group-horizontal">
-                    <li class="list-group-item flex-fill">
-                            <div class="d-flex w-100 justify-content-between">
-                                
+        <div>            
+            <div class="container-fluid">
+                <div class="row">
+                    <Sidebar />
 
-                                <img class="row g-0" src="./f80m3.webp" alt="..." height="100" />
-                                
-                            </div>
+                    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-5">
+                    <div class="d-flex justify-content-between flex-wrap align-items-center pt-3 pb-2 mb-3">
                         
-                    </li>
-                    <li class="list-group-item flex-fill">
+                        <h1 class="h2">Car Inventory</h1>
 
-                            <p class="mb-1">{car.customerName}</p>
-                    </li>
-                    <li class="list-group-item flex-fill">
-                            <p class="mb-1">{car.year} {car.make} {car.model}</p>
-                    </li>
-                    <li class="list-group-item flex-fill">
-                            <small class="text-body-secondary">VIN: {car.vin}</small>
-                        {/* </Link> */}
-                        </li>
-                        </ul>
-                    )}
-                {/* </ol> */}
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+                            {carInventory.map((carItem) => 
+                                <Link to ={`/cars/${carItem.id}`}>
+                                    <div class="card h-100">
+                                        <img src="./f80m3.webp" class="card-img-top" alt="..."/>
+                                        
+                                        <div class="card-body text-start">
+                                            <p class="card-title">{carItem.customerName}
+                                            </p>
+                                            <p class="card-text">{carItem.year} {carItem.make} {carItem.model}</p>
+                                            <p class="card-text text-muted">
+                                                VIN:{carItem.vin}</p>
+                                        </div>
+                                    </div>  
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+
+                    
+                    </main>
+                </div>
             </div>
         </div>
     )
