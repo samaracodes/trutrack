128f086b1d1b76636aca6f91fd9987858faacb11
Sam <samaracodes@gmail.com>
2023-08-15 22:14:29 -0400

    navbar styling & brand logo import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,31 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-sm bg-body-tertiary">
+            <nav class="navbar navbar-expand-md navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/">TruTrack</a>
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/cars">Car Inventory</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/repair-orders">Repair Orders</a>
-                            </li>
-                            
-                        </ul>
-                    </div>
+                <a class="navbar-brand" href="/">
+                    <img src="./brand-logo-trnsp.png" alt="truTrack brand logo" class="brand-logo d-inline-block align-text-top"/>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/cars">Car Inventory</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/repair-orders" >Repair Orders</a>
+                    </li>
+                    </ul>
+                    <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
                 </div>
             </nav>
         </div>
