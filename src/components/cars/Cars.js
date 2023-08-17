e13392d418bbc8f0b821fe4a8c2d231ad72b415e
Sam <samaracodes@gmail.com>
2023-08-17 02:12:43 -0400

    ui styling tweaks

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -13,19 +13,19 @@ const Cars = ({ carInventory }) => {
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-5">
                     <div className="d-flex justify-content-between flex-wrap align-items-center pt-3 pb-2 mb-3">
                         
-                        <h1 className="h2">Car Inventory</h1>
+                        <h2 className="h2 mb-3">Car Inventory</h2>
 
                         <div className="row row-cols-1 row-cols-md-3 g-4">
                             {carInventory.map((carItem) => 
                                 <Link to ={`/cars/${carItem.id}`} key={carItem.id}>
-                                    <div key={carItem.id} className="card h-100">
-                                        <img src="./f80m3.webp" className="card-img-top" alt="..."/>
+                                    <div key={carItem.id} className="card h-100 p-3">
+                                        <img src="./f80m3.webp" className="card-img-top mx-auto img-thumbnail" alt="car image" />
                                         
-                                        <div className="card-body text-start">
-                                            <p className="card-title">{carItem.customerName}
+                                        <div className="card-body text-start mt-2">
+                                            <p className="card-title mb-0">{carItem.customerName}
                                             </p>
-                                            <p className="card-text">{carItem.year} {carItem.make} {carItem.model}</p>
-                                            <p className="card-text text-muted">
+                                            <p className="card-text mb-0">{carItem.year} {carItem.make} {carItem.model}</p>
+                                            <p className="card-text text-muted ">
                                                 VIN:{carItem.vin}</p>
                                         </div>
                                     </div>  
