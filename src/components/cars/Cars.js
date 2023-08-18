1a67fa545a311b97705b9fc6ebb78bdb62d587c7
Sam <samaracodes@gmail.com>
2023-08-17 22:44:15 -0400

    addition to car component - button to create new vehicle

--- src/components/cars/Cars.js
+++ src/components/cars/Cars.js
@@ -15,6 +15,15 @@ const Cars = ({ carInventory }) => {
                         
                         <h2 className="h2 mb-3">Car Inventory</h2>
 
+                        <Link to="/cars/new">
+                            <button type="button" class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill me-1" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"></path>
+                                </svg>
+                                New Vehicle
+                            </button>
+                        </Link>
+
                         <div className="row row-cols-1 row-cols-md-3 g-4">
                             {carInventory.map((carItem) => 
                                 <Link to ={`/cars/${carItem.id}`} key={carItem.id}>
