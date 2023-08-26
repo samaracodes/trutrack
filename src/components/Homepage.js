35731cee8493664d8a6372175fdc8c53aa67390b
Sam <samaracodes@gmail.com>
2023-08-26 17:32:48 -0400

    homepage ui styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,85 +5,51 @@ const Homepage = () => {
         <div class="">
             
             <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-
-                    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
 
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                            <img src="./bmwf1-pit-crew.jpg" class="d-block w-100"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"/>
+                            <img src="./bmwf1-pit-crew.jpg" class="d-block w-100"  ariaHidden="true" preserveAspectRatio="xMidYMid slice" focusable="false" style={{"height": "650px"}}/>
 
                             <div class="container">
                             <div class="carousel-caption text-start">
-                                <h1>Example headline.</h1>
-                                <p>Some representative placeholder content for the first slide of the carousel.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                                <img src="./brand-logo-trnsp.png" class="w-50"/>                                
+                                <hr/>
+                                <h5>Automotive dealer management system software designed to help track vehicle repairs.</h5>
+                                
+                                {/* <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p> */}
                             </div>
                             </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="./bmwf1-pit-crew.jpg" class="d-block w-100"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-
-                            <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Another example headline.</h1>
-                                <p>Some representative placeholder content for the second slide of the carousel.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-                            </div>
-                            </div>
-                    </div>
-                    <div class="carousel-item">
-                    <img src="./bmwf1-pit-crew.jpg" class="d-block w-100"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"/>
-
-                            <div class="container">
-                            <div class="carousel-caption text-end">
-                                <h1>One more for good measure.</h1>
-                                <p>Some representative placeholder content for the third slide of this carousel.</p>
-                                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-                            </div>
-                            </div>
-                    </div>
+                    
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
 
             <div class="container marketing">
 
                 {/* <!-- Three columns of text below the carousel --> */}
-                <div class="row">
+                <div class="row pt-5">
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="content1.jpg" />
 
-                        <h2>Heading</h2>
-                        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-                        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        <h2>Efficiency</h2>
+                        <p>TruTrack assists service writers and technicians keep track of the vehicles that come in for repair.</p>
+                        <p><a class="btn btn-warning" href="#">View details &raquo;</a></p>
                     </div>
 
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="content2.png" />
 
-                        <h2>Heading</h2>
-                        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-                        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        <h2>Organization</h2>
+                        <p>Vehicle History details are easily stored and accessible all in one place. Updates are uploaded in real time 😊</p>
+                        <p><a class="btn btn-warning" href="#">View details &raquo;</a></p>
                     </div>
 
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                        <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="content3.jpg" />
 
-                        <h2>Heading</h2>
-                        <p>And lastly this, the third column of representative placeholder content.</p>
-                        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+                        <h2>Coverage</h2>
+                        <p>We are able to view warranty coverage and parts that are on order.</p>
+                        <p><a class="btn btn-warning" href="#">View details &raquo;</a></p>
                     </div>
                 </div>
 
@@ -97,7 +63,7 @@ const Homepage = () => {
                     <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
                 </div>
                 <div class="col-md-5">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" ariaLabel="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
 
                 </div>
                 </div>
@@ -110,7 +76,7 @@ const Homepage = () => {
                     <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" ariaLabel="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
 
                 </div>
                 </div>
@@ -123,7 +89,7 @@ const Homepage = () => {
                     <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
                 </div>
                 <div class="col-md-5">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" ariaLabel="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
 
                 </div>
                 </div>
