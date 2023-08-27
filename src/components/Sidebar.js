557390e2dc2c174bcf441bd6f900f8311aa61239
Sam <samaracodes@gmail.com>
2023-08-26 20:28:30 -0400

    changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
                 <li className="sbni nav-item">
                     <NavLink to="/" className="nav-link" style={{ textDecoration: 'none' }} aria-current="page" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home me-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                    Dashboard
+                    Home
                     </NavLink>
                 </li>
                 <hr/>
@@ -17,7 +17,7 @@ const Sidebar = () => {
                 <li className="sbni nav-item">
                     <NavLink to="/cars" className="nav-link" style={{ textDecoration: 'none' }}  href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-users me-2" aria-hidden="true"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                    Inventory
+                    Customers
                     </NavLink>
                 </li>
                 <hr/>
@@ -25,7 +25,15 @@ const Sidebar = () => {
                 <li className="sbni nav-item">
                     <NavLink to="/repair-orders" className="nav-link" style={{ textDecoration: 'none' }}  href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-bar-chart-2 me-2" aria-hidden="true"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
-                    Repair Orders
+                    Open RO's
+                    </NavLink>
+                </li>
+                <hr/>
+
+                <li className="sbni nav-item">
+                    <NavLink to="/repair-orders" className="nav-link" style={{ textDecoration: 'none' }}  href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-bar-chart-2 me-2" aria-hidden="true"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
+                    Closed RO's
                     </NavLink>
                 </li>
                 <hr/>
