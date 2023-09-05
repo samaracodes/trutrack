e8c5462bf6217fafd56c866399ff561ba56394f0
Sam <samaracodes@gmail.com>
2023-09-04 22:55:24 -0400

    renamed components/routes from cars to customer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { React, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
-import Cars from './components/cars/Cars'
-import CarItem from './components/cars/CarItem'
-import CarForm from './components/cars/CarForm';
+import Customers from './components/customers/Customers'
+import CustomerItem from './components/customers/CustomerItem'
+import CustomerForm from './components/customers/CustomerForm';
 import RepairOrders from './components/repairorders/RepairOrders';
 import RepairOrderId from './components/repairorders/RepairOrderId';
 import RepairOrderForm from './components/repairorders/RepairOrderForm';
@@ -13,14 +13,14 @@ import RepairOrderEditForm from './components/repairorders/RepairOrderEditForm';
 
 
 function App() {
-  const [cars, setCars] = useState([])
+  const [customers, setCustomers] = useState([])
   const [repairOrders, setRepairOrders] = useState([])
 
   useEffect(() => {
-    fetch("http://localhost:3001/car-inventory")
+    fetch("http://localhost:3001/customers")
     .then((response) => response.json())
-    .then((info) => {
-      setCars(info)
+    .then((customer) => {
+      setCustomers(customer)
     })
   }, [])
 
@@ -61,9 +61,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
 
-        <Route path="/cars" element={<Cars carInventory={cars}/>} />
-        <Route path="/cars/:id" element={<CarItem />} />
-        <Route path="/cars/new" element={<CarForm />} />
+        <Route path="/customers" element={<Customers customers={customers}/>} />
+        <Route path="/customers/:id" element={<CustomerItem />} />
         
         <Route path="/repair-orders" element={<RepairOrders repairOrders={repairOrders} />} />
         <Route path="/repair-orders/:id" element={<RepairOrderId onDeleteRO={onDeleteRO} />} />
