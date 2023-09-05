1d64500f9f415e2deb559449760f4b45d313378b
Sam <samaracodes@gmail.com>
2023-09-05 10:20:28 -0400

    sidebar nav link path change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,7 +15,7 @@ const Sidebar = () => {
                 <hr/>
 
                 <li className="sbni nav-item">
-                    <NavLink to="/cars" className="nav-link" style={{ textDecoration: 'none' }}  href="#">
+                    <NavLink to="/customers" className="nav-link" style={{ textDecoration: 'none' }}  href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-users me-2" aria-hidden="true"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                     Customers
                     </NavLink>
