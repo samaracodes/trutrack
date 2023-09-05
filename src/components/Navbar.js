37f631a636b09ae9ca22369f8678a637f7bb08cf
Sam <samaracodes@gmail.com>
2023-09-05 10:16:12 -0400

    implemented NavLink, replace url paths

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-md navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
+                    <a className="navbar-brand" href="/trutrack">
                         <img src="./brand-logo-trnsp.png" alt="truTrack brand logo" className="brand-logo d-inline-block align-text-top"/>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,13 +16,13 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/customers">Customers</a>
+                        <NavLink className="nav-link" to="/customers">Customers</NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/repair-orders" >Repair Orders</a>
+                        <NavLink className="nav-link" to="/repair-orders" >Repair Orders</NavLink>
                     </li>
                     </ul>
                     <form className="d-flex">
